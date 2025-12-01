721e95f67381051b5e5d674457e6b95a39c057b9
akamar0 <jsakamarobrtt@gmail.com>
2025-12-01 16:27:33 +0800

    Add files via upload

--- DsaProject.cpp
+++ DsaProject.cpp
@@ -61,8 +61,7 @@ int main() {
     int choice;
     string title, artist;
 
-    do {
-  
+ do {
 
     cout << "                                                                    \n";
     cout << "                                                                    \n";
@@ -72,94 +71,88 @@ int main() {
     cout << "                                                                    \n";
     cout << "                                                                    \n";
 
-    cout << "==================== PLAYLIST MANAGER MENU ====================\n";
+    cout << "♪ =================== ♪  PLAYLIST MANAGER MENU ♪ ==================== ♪ \n";
 
-        cout << "[1.] Add Song to Playlist\n";
-        cout << "[2.] Display Playlist\n";
-        cout << "[3.] Add Song to Queue\n";
-        cout << "[4.] Play Next Song\n";
-        cout << "[5.] Previous Song\n";
-        cout << "[6.] Show Current Song\n";
-        cout << "[7.] Exit\n";
-        cout << "Select an option: ";
-        cin >> choice;
+    cout << "[1.] Add Song to Playlist\n";
+    cout << "[2.] Display Playlist\n";
+    cout << "[3.] Add Song to Queue\n";
+    cout << "[4.] Play Next Song\n";
+    cout << "[5.] Previous Song\n";
+    cout << "[6.] Show Current Song\n";
+    cout << "[7.] Exit\n";
+    cout << "Select an option: ";
 
-        cin.ignore();
+    // FIXED INPUT HANDLING
+    if (!(cin >> choice)) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "\n[Choose from choices only!]\n\n";
+        continue;   // go back to menu
+    }
+    cin.ignore(1000, '\n');
 
-        switch (choice) {
-            case 1: {
-                cout << "Enter song title: ";
-                getline(cin, title);
-                cout << "Enter artist: ";
-                getline(cin, artist);
-                addSong(playlist, title, artist);
-                cout << "SONG SUCCESSFULLY ADDED!\n";
-                break;
-            }
-            case 2:
-                displayPlaylist(playlist);
-                break;
-
-            case 3: {
-                cout << "Enter song title to queue: ";
-                getline(cin, title);
-
-                Song* s = findSong(playlist, title);
-                if (s) {
-                    playQueue.push(s);
-                cout << s->title << " added to play queue.\n";
-                } else {
-                    cout << "Song not found in playlist!!\n";
-                }
-                break;
-            }
-
-            case 4: {
-                if (playQueue.empty()) {
-
-                    cout << "No songs in queue.\n";
-
-                } else {
-                    if (currentSong) history.push(currentSong);
-
-                    currentSong = playQueue.front();
-                    playQueue.pop();
-                    cout << "Now playing: \n"<< currentSong->title << endl;
-
-                }
-                break;
-            }
-
-            case 5: {
-                if (history.empty()) {
-                    cout << "No previous song.\n";
-                } else {
-                    playQueue.push(currentSong);
-                    currentSong = history.top();
-                    history.pop();
-                    cout << "Previous: " << currentSong->title << endl;
-                }
-                break;
-            }
-
-            case 6:
-                if (currentSong)
-
-                    cout << "Current song: " << currentSong->title << " - " << currentSong->artist << endl;
-                else
-                    cout << "No song is currently playing as of the moment.\n";
-
-                break;
-
-            case 7:
-                cout << "Exiting...\n";
-                break;
-
-            default:
-                cout << "Invalid choice try again!.\n";
+    switch (choice) {
+        case 1: {
+            cout << "Enter song title ♬ : ";
+            getline(cin, title);
+            cout << "Enter artist ♬ : ";
+            getline(cin, artist);
+            addSong(playlist, title, artist);
+            cout << "♪ SONG SUCCESSFULLY ADDED! ♪\n";
+            break;
         }
+        case 2:
+            displayPlaylist(playlist);
+            break;
+        case 3: {
+            cout << "♬ Enter song title to queue: ";
+            getline(cin, title);
 
-    } while (choice != 7);
+            Song* s = findSong(playlist, title);
+            if (s) {
+                playQueue.push(s);
+                cout << s->title << " added to queue.\n";
+            } else {
+                cout << "[Song not found!]\n";
+            }
+            break;
+        }
+        case 4: {
+            if (playQueue.empty()) {
+                cout << "[Queue is empty]\n";
+            } else {
+                if (currentSong) history.push(currentSong);
+                currentSong = playQueue.front();
+                playQueue.pop();
+                cout << "Now playing: " << currentSong->title << " ♬\n";
+            }
+            break;
+        }
+        case 5: {
+            if (history.empty()) {
+                cout << "[No previous song]\n";
+            } else {
+                playQueue.push(currentSong);
+                currentSong = history.top();
+                history.pop();
+                cout << "Previous: " << currentSong->title << " ♬\n";
+            }
+            break;
+        }
+        case 6:
+            if (currentSong)
+                cout << "Current: " << currentSong->title << " - " << currentSong->artist << endl;
+            else
+                cout << "[No song playing]\n";
+            break;
 
-    return 0;
+        case 7:
+            cout << "Exiting...\n";
+            break;
+
+        default:
+            cout << "[Invalid choice!]\n";
+    }
+
+} while (choice != 7);
 }
